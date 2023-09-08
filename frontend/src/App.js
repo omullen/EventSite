4f621d2a1caf569566db70aa5df45c19266215b5
omullen <lavendericana@gmail.com>
2023-09-08 18:23:48 +0300

    add bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div>
-      <div>
+      <Header />
         <h1>Welcome to EventSite!</h1>
-      </div>
+      <Footer />
     </div>
   );
 }
