d68534ed3c9ce53aef7568e5426747bf938ee6b9
omullen <lavendericana@gmail.com>
2023-09-12 01:20:07 +0300

    homescreen update

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,12 +2,12 @@ import React from 'react'
 import {Col, Row} from 'react-bootstrap'
 import events from '../events'
 import Event from '../components/Event'
-import {motion} from 'framer-motion'
 
 const HomeScreen = () => {
   return (
     <>
     <h1>Events</h1>
+    <p>Find the fun!</p>
     <Row>
         {events.map(e =>(
             <Col sm={12} md={6} lg={4} xl={3}>
